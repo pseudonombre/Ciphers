2e192a75c3a0bbd8724c36c40e3e401e0fe40716
Anderson <andersoncd24@gcc.edu>
2026-09-17 18:15:27 -0400

    refactored aliases and their documentation

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.channels.spi.AbstractSelectionKey;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -10,8 +11,8 @@ public class Main {
     private static final Path SAVED_TEXT_DIR = Path.of("src", "SavedText");
     // Module takes: input string, arguments
 
-    private static final Map<String, BiFunction<String, String, String>> MODULES =
-            new HashMap<>();
+    private static final Map<String, BiFunction<String, String, String>> MODULES = new HashMap<>();
+    private static final Map<String, String> ALIASES = new HashMap<>();
     static String cws = "";
 
     public static void main(String[] args) {
@@ -40,7 +41,15 @@ public class Main {
             }
             if (instruction.equalsIgnoreCase("mods")) {
                 for (String module : MODULES.keySet()) {
-                    System.out.println(module);
+                    System.out.print(module);
+
+                    for (Map.Entry<String, String> alias : ALIASES.entrySet()) {
+                        if (alias.getValue().equals(module)) {
+                            System.out.print(" (" + alias.getKey() + ")");
+                        }
+                    }
+
+                    System.out.println();
                 }
                 continue;
             }
@@ -72,22 +81,24 @@ public class Main {
     private static void loadModules() {
         MODULES.put("echo", (input, argString) -> input);
         MODULES.put("group", GroupChars::function);
-        MODULES.put("remws", (input, argString) -> input.replaceAll("\\s+", ""));
         MODULES.put("removewhitespace", (input, argString) -> input.replaceAll("\\s+", ""));
         MODULES.put("freqa", FreqAnalysis::function);
         MODULES.put("ic", IC::function);
         MODULES.put("cosetic", CosetIC::function);
         MODULES.put("ceasarshift", CeasarShift::function);
-        MODULES.put("cs", CeasarShift::function);
-        MODULES.put("vigcoset", VigenereCosetShift::function);
-        MODULES.put("vigcosetshift", VigenereCosetShift::function);
         MODULES.put("vigenerecosetshift", VigenereCosetShift::function);
         MODULES.put("vigenere", Vigenere::function);
-        MODULES.put("veginere", Vigenere::function);
-        MODULES.put("vig", Vigenere::function);
-        MODULES.put("sw", SplitWords::function);
+        MODULES.put("splitwords", SplitWords::function);
         MODULES.put("read", Read::function);
         MODULES.put("write", Write::function);
+
+        ALIASES.put("remws", "removewhitespace");
+        ALIASES.put("cs", "ceasarshift");
+        ALIASES.put("vigcoset", "vigenerecosetshift");
+        ALIASES.put("vigcosetshift", "vigenerecosetshift");
+        ALIASES.put("veginere", "vigenere");
+        ALIASES.put("vig", "vigenere");
+        ALIASES.put("sw", "splitwords");
     }
 
     private static void executeInstruction(String instruction) throws IOException {
@@ -100,6 +111,7 @@ public class Main {
     }
 
     private static String runModule(String moduleName, String input, String moduleArgs) {
+        moduleName = ALIASES.getOrDefault(moduleName, moduleName);
         BiFunction<String, String, String> module = MODULES.get(moduleName.toLowerCase());
 
         if (module == null) {
